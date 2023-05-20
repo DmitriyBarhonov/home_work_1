608d87ce57d384365d8deb69ff3961fb5acf92bc
Иванович Иванов <explay167@gmail.com>
2023-05-20 15:57:45 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,24 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            if (action.payload === "up") {
+                return state.sort((a: UserType, b: UserType)=> a.name > b.name ? 1: -1 )  
+            }
+            if (action.payload === "down") {
+                return state.sort((a: UserType, b: UserType)=> a.name < b.name ? 1: -1 )  
+            }
 
             return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter(el => el.age >= 18 ? el : null) // need to fix
         }
         default:
             return state
